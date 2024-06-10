ebd765baa2e8cb74b4eb99d4e75816916f01a39d
Ivan Valeff <ivaleff@gmail.com>
2024-06-10 11:40:31 -0300

    single login and register

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import RegisterForm from "./RegisterForm";
+
+
 const LoginForm = () => {
+
+  const [login, setLogin] = useState(true);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -15,7 +21,9 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <>
+    {login ?
+      (<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 className="text-xl font-bold leading-tight text-center tracking-tight md:text-2xl dark:text-white">
             Inicia sesion
@@ -42,13 +50,17 @@ const LoginForm = () => {
           </div>
           <button type="submit" className="w-full mt-6 bg-indigo-600 hover:bg-blue-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Iniciar sesión</button>
           <p className="text-sm font-light text-gray-500 dark:text-gray-400 flex justify-center">
-            <button className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+            <button className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={()=>{setLogin(false)}}>
               No tengo cuenta, quiero registrarme
             </button>
           </p>
         </form>
       </div>
-    </div>
+    </div>) : 
+      (<RegisterForm/>)
+    }
+    
+    </>
   );
 };
 
--- src/Components/SideBar/SideBarLeft-notLogged.jsx
+++ src/Components/SideBar/SideBarLeft-notLogged.jsx
@@ -1,14 +1,14 @@
-import ConfirmationCode from "../Login/ConfirmationCode.jsx";
+
 import LoginForm from "../Login/LoginForm.jsx";
-import RegisterForm from "../Login/RegisterForm.jsx";
-const SideBarLogin = () => {
+
+
+const SideBarLogin = () => { 
+
   return (
     <div className="h-full px-3 py-4 overflow-y-auto fixed top-0 left-0 z-40 w-96 hidden sm:fixed sm:h-screen lg:block md:block sm:bg-white sm:border-l sm:border-r dark:sm:bg-gray-800 dark:sm:border-gray-700">
-      <LoginForm/>
-      <br />
-      <RegisterForm/>
-      <br />
-      <ConfirmationCode/>
+
+    <LoginForm />;
+      
     </div>
   );
 };
